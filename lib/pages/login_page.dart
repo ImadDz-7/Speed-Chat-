ce93e9859aa4ac356d465aa478ba2fa517f187c2
ImadDz-7 <imadbouamrirene97@gmail.com>
2025-07-19 17:45:49 +0000

    Add backgroundColor

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -6,6 +6,7 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: const Color(0xFF2B475E),
       body: Column(
         children: [],
       ),
