c42c5099cae0fee9064b0640b5c25e0a056a3d05
ImadDz-7 <imadbouamrirene97@gmail.com>
2025-07-19 17:32:50 +0000

    Create LoginPage widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:speed_chat_app/pages/login_page.dart';
 
 void main() {
   runApp(const SpeedChat());
@@ -11,7 +12,7 @@ class SpeedChat extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(),
+      home: LoginPage(),
     );
   }
 }
